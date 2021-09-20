60185a880183654b5297161f85eaf0a9dd53dd7c
Ramsey <seamusapple@163.com>
2021-09-20 23:11:37 +0800

    Add methods for linkedList

--- Swift/Algorithms.playground/Pages/1.2 Basic Struct.xcplaygroundpage/Contents.swift
+++ Swift/Algorithms.playground/Pages/1.2 Basic Struct.xcplaygroundpage/Contents.swift
@@ -5,6 +5,7 @@ import Foundation
 testStack()
 testQueue()
 testImprovedQueue()
+testLinkedList()
 
 // Mark: - Test Stack
 func testStack() {
@@ -78,5 +79,87 @@ func testImprovedQueue() {
     names.dequeue()
     print(names.count)
     print(names)
+}
+
+func testLinkedList() {
+    let list = LinkedList<String>()
+    list.isEmpty   // true
+    list.first     // nil
+    list.count     // 0
+
+    list.appendToTail("Swift")
+    list.isEmpty         // false
+    list.first!.value    // "Swift"
+    list.last!.value     // "Swift"
+    list.count           //1
+
+    list.appendToTail("is")
+    list.first!.value    // "Swift"
+    list.last!.value     // "is"
+    list.count           // 2
+
+    list.appendToTail("great")
+    list.first!.value    // "Swift"
+    list.last!.value     // "great"
+    list.count           // 3
+
+    print(list)
+    //[0]Swift
+    //[1]is
+    //[2]Great
+
+    list.node(at: 0)?.value // Swift
+    list.node(at: 1)?.value // is
+    list.node(at: 2)?.value // great
+    list.node(at: 3)?.value // nil
+
+
+    list.insert("language", at: 1)
+    print(list)
+    //[0]Swift
+    //[1]language
+    //[2]is
+    //[3]great
+
+
+    list.removeNode(list.first!)
+    print(list)
+    //[0]language
+    //[1]is
+    //[2]great
+
+
+    list.remove(at: 1)
+    print(list)
+    //[0]language
+    //[1]great
+
+    list.removeLast()
+    print(list)
+    //[0]language
+
+    list.insertToHead("study")
+    list.count             // 2
+    print(list)
+    //[0]study
+    //[1]language
+
+
+    list.removeAll()
+    print(list) //linked list is empty
+
+    list.insert("new", at: 3)
+    print(list)
+    //[0]new
+
+    list.insert("new", at: 3) //out of range
+    print(list)
+    //[0]new
+
+    list.insert("new", at: 1)
+    print(list)
+    //[0]new
+    //[1]new
+
 }
 //: [Next](@next)
--- Swift/Algorithms.playground/Pages/1.2 Basic Struct.xcplaygroundpage/Sources/LinkedListNode.swift
+++ Swift/Algorithms.playground/Pages/1.2 Basic Struct.xcplaygroundpage/Sources/LinkedListNode.swift
@@ -2,12 +2,20 @@ import Foundation
 
 public class LinkedListNode<Element> {
     
-    var value: Element
-    weak var previou: LinkedListNode?
-    var next: LinkedListNode?
+    public var value: Element
+    public weak var previous: LinkedListNode?
+    public var next: LinkedListNode?
     
-    init(value: Element) {
+    public init(value: Element) {
         self.value = value
+    }
+}
+
+extension LinkedListNode: CustomStringConvertible {
+    public var description: String {
+        return previous == nil
+            ? "First and Current: \(value)"
+            : next == nil ? "Previous: \(previous!.value), Current: \(value) and Last" : "Previous: \(previous!.value), Current: \(value), Next: \(next!.value)"
     }
 }
 
@@ -32,7 +40,7 @@ public class LinkedList<Element> {
         guard !isEmpty else { return nil }
         
         var last = head
-        while let next = head?.next {
+        while let next = last?.next {
             last = next
         }
         return last
@@ -41,7 +49,7 @@ public class LinkedList<Element> {
     public func node(at index: Int) -> Node? {
         if index == 0 { return head }
         
-        var node = head
+        var node = head?.next
         guard index < count else { return nil }
         for _ in 1..<index where node != nil {
             node = node?.next
@@ -49,10 +57,11 @@ public class LinkedList<Element> {
         return node
     }
     
+    // Insert Node
     public func appendToTail(_ value: Element) {
         let newNode = Node(value: value)
         if let lastNode = last {
-            newNode.previou = lastNode
+            newNode.previous = lastNode
             lastNode.next = newNode
         } else {
             head = newNode
@@ -65,10 +74,98 @@ public class LinkedList<Element> {
             head = newHead
         } else {
             newHead.next = head
-            head?.previou = newHead
+            head?.previous = newHead
             head = newHead
         }
     }
     
+    public func insert(_ value: Element, at index: Int) {
+        guard index >= 0 else {
+            print("\nInvalid Input Index")
+            return
+        }
+        
+        let newNode = Node(value: value)
+        
+        guard !isEmpty else { head = newNode; return }
+        
+        if index == 0 {
+            insertToHead(value)
+        } else {
+            guard index <= count else {
+                print("\nIndex Out Of Range")
+                return
+            }
+            
+            let previous = node(at: index-1)
+            let next = previous?.next
+            previous?.next = newNode
+            newNode.previous = previous
+            newNode.next = next
+            next?.previous = newNode
+        }
+    }
+    
+    // Remove Node
+    public func removeAll() {
+        head = nil
+    }
+    
+    public func removeLast() -> Element? {
+        guard !isEmpty else { return nil }
+        return removeNode(last!)
+    }
+    
+    public func removeNode(_ node: Node) -> Element? {
+        guard head != nil else {
+            print("\nLinked list is Empty")
+            return nil
+        }
+        
+        let prev = node.previous
+        let next = node.next
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        next?.previous = prev
+        node.previous = nil
+        node.next = nil
+        
+        return node.value
+    }
+    
+    public func remove(at index: Int) -> Element? {
+        guard head != nil else {
+            print("\nLinked list is Empty")
+            return nil
+        }
+        
+        guard let node = node(at: index) else { return nil }
+        return removeNode(node)
+    }
+    
+    public init() { }
+    
     private var head: Node?
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        return printAllNodes()
+    }
+    
+    private func printAllNodes() -> String {
+
+        guard head != nil else { return "\nLinked list is Empty" }
+
+        var description = "\nStart printing all nodes:"
+        var node = head
+        for index in 0..<count where node != nil {
+            description.append("\n[\(index)]: \(node!.description)")
+            node = node!.next
+        }
+        return description
+    }
 }
